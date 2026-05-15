ac193c37c531e02c2e3934aff7a0260ffbc3f70b
Bhavya-Tadepalli <lakshmitadepalli761@gmail.com>
2026-05-16 03:28:51 +0530

    final fixing of issue

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,41 +1,45 @@
 using backend.Data;
 using Microsoft.EntityFrameworkCore;
 
+
 var builder = WebApplication.CreateBuilder(args);
 
+// Add services to the container.
+
 builder.Services.AddControllers();
-
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseMySQL(
-        builder.Configuration.GetConnectionString("DefaultConnection")
-    ));
-
-// ADD CORS
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("AllowAll",
-        policy =>
-        {
-            policy.AllowAnyOrigin()
-                  .AllowAnyMethod()
-                  .AllowAnyHeader();
-        });
-});
 
 builder.Services.AddEndpointsApiExplorer();
 
 builder.Services.AddSwaggerGen();
 
+// CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend",
+        policy =>
+        {
+            policy.WithOrigins("https://jocular-heliotrope-c0bd69.netlify.app")
+                  .AllowAnyHeader()
+                  .AllowAnyMethod();
+        });
+});
+
+// MySQL Connection
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseMySQL(
+        builder.Configuration.GetConnectionString("DefaultConnection")
+    ));
+
 var app = builder.Build();
+
+// Configure the HTTP request pipeline.
 
 app.UseSwagger();
 
 app.UseSwaggerUI();
 
-app.UseHttpsRedirection();
-
-// ENABLE CORS
-app.UseCors("AllowAll");
+// Enable CORS
+app.UseCors("AllowFrontend");
 
 app.UseAuthorization();
 
